a2a05b671dbdc2eed19d31d33dd48013a84b7c83
adnanhamidbeigh <adnanhamidbeg@gmail.com>
2024-08-20 16:30:23 +0000

    DSCEngine test

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {DecentralisedStableCoin} from "src/DecentralisedStableCoin.sol";
+import {DeployDSC} from "script/DeployDSC.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DecentralisedStableCoin dsc;
+    DSCEngine dscEngine;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address weth;
+    address public USER = makeAddr("USER");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dscEngine, config) = deployer.run();
+        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
+    /*//////////////////////////////////////////////////////////////
+                              PRICE TESTS
+    //////////////////////////////////////////////////////////////*/
+
+    function testGetUsdValue() external view {
+        uint256 ethAmount = 15e18;
+        // 15e18 * 2000/1e18 = 30,000e18
+        uint256 expectedUsd = 30_000e18;
+        uint256 actualUsd = dscEngine.getUsdValue(weth, ethAmount);
+        assertEq(actualUsd, expectedUsd);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              DEPOSIT COLLATERAL TESTS
+    //////////////////////////////////////////////////////////////*/
+    function testRevertIfCollateralZero() external {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dscEngine), AMOUNT_COLLATERAL);
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dscEngine.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+}
